461413aa5f7d189e8ac7b090e71b1e59828e1cbb
lebohang0824 <lebohang0824@gmail.com>
2020-02-18 10:36:55 +0200

    Updated specs

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,6 +1,12 @@
 describe('Testing database query language', () => {
 
-	const { addNewVisitor, listAllVisitors, deleteVisitor } = require('../src/app.js'); 
+	const { 
+		addNewVisitor,
+		listAllVisitors,
+		deleteVisitor,
+		updateVisitor,
+		viewOneVisitor,
+		deleteAllVisitors } = require('../src/app.js');
 
 	const visitor = {
 		name: 'Porche 911',
@@ -11,54 +17,91 @@ describe('Testing database query language', () => {
 		comments: 'Nomvelo'
 	}
 
-	let id = 0;
+	const newName = "Thabo Mahlaba";
+	const newAge = 55;
+	const newComments = "Beautiful place";
 
-	it('Add visitor', async done => {
-		await addNewVisitor(visitor.name, visitor.age, visitor.date, visitor.time, visitor.assistant, visitor.comments)
-			.then(res => {
-				const objVisitor = res.rows
-				expect(objVisitor[0].name).toBe(visitor.name);
-				expect(objVisitor[0].age).toBe(visitor.age);
-				expect(objVisitor[0].date_of_visit).toEqual(visitor.date);
-				expect(objVisitor[0].time_of_visit).toBe(visitor.time);
-				expect(objVisitor[0].assistant).toBe(visitor.assistant);
-				expect(objVisitor[0].comments).toBe(visitor.comments);
-			});
+	it('Should add new visitors', async() => {
 
-		done();
+		const newVisitor = await addNewVisitor(
+			visitor.name, 
+			visitor.age, 
+			visitor.date, 
+			visitor.time, 
+			visitor.assistant, 
+			visitor.comments
+		);
+
+		expect(newVisitor[0].name).toEqual(visitor.name);
+		expect(newVisitor[0].age).toEqual(visitor.age);
+		expect(newVisitor[0].date_of_visit).toEqual(visitor.date);
+		expect(newVisitor[0].time_of_visit).toEqual(visitor.time);
+		expect(newVisitor[0].assistant).toEqual(visitor.assistant);
+		expect(newVisitor[0].comments).toEqual(visitor.comments);
 	});
 
-	it('List all vistors', async done => {
-		await listAllVisitors().then(res => {
-			const objVisitor = res.rows
+	it('Should get all visitors', async() => {
+		const allVisitors = await listAllVisitors();
 
-			expect(objVisitor[0].name).toBe(visitor.name);
-			expect(objVisitor[0].age).toBe(visitor.age);
-			expect(objVisitor[0].date_of_visit).toEqual(visitor.date);
-			expect(objVisitor[0].time_of_visit).toBe(visitor.time);
-			expect(objVisitor[0].assistant).toBe(visitor.assistant);
-			expect(objVisitor[0].comments).toBe(visitor.comments);
-
-			id = objVisitor.id;
-		});
-		
-		done();
+		expect(allVisitors).not.toEqual([]);
 	});
 
-	it('Deletegit  vistors', async done => {
+	it('Should update visitor', async() => {
 
-		await deleteVisitor(id).then(res => {
-			const objVisitor = res.rows
+		const allVisitors = await listAllVisitors();
+		const id = allVisitors[0].id;
 
-			expect(objVisitor[0].name).toBe(visitor.name);
-			expect(objVisitor[0].age).toBe(visitor.age);
-			expect(objVisitor[0].date_of_visit).toEqual(visitor.date);
-			expect(objVisitor[0].time_of_visit).toBe(visitor.time);
-			expect(objVisitor[0].assistant).toBe(visitor.assistant);
-			expect(objVisitor[0].comments).toBe(visitor.comments);
-		});
-		
-		done();
+		const updateOneVisitor = await updateVisitor(
+			id,
+			newName, 
+			newAge, 
+			visitor.date, 
+			visitor.time, 
+			visitor.assistant, 
+			newComments
+		);
+
+		expect(updateOneVisitor[0].name).toEqual(newName);
+		expect(updateOneVisitor[0].age).toEqual(newAge);
+		expect(updateOneVisitor[0].date_of_visit).toEqual(visitor.date);
+		expect(updateOneVisitor[0].time_of_visit).toEqual(visitor.time);
+		expect(updateOneVisitor[0].assistant).toEqual(visitor.assistant);
+		expect(updateOneVisitor[0].comments).toEqual(newComments);
+	}); 
+
+	it('Should view one visitor', async() => {
+		const allVisitors = await listAllVisitors();
+		const id = allVisitors[0].id;
+
+		const oneVisitor = await viewOneVisitor(id);
+
+		expect(oneVisitor[0].name).toEqual(newName);
+		expect(oneVisitor[0].age).toEqual(newAge);
+		expect(oneVisitor[0].date_of_visit).toEqual(visitor.date);
+		expect(oneVisitor[0].time_of_visit).toEqual(visitor.time);
+		expect(oneVisitor[0].assistant).toEqual(visitor.assistant);
+		expect(oneVisitor[0].comments).toEqual(newComments);
+	});  
+ 
+	it('Should delete visitor', async() => {
+
+		const allVisitors = await listAllVisitors();
+		const id = allVisitors[0].id;
+
+		const deleteVisitors = await deleteVisitor(id);
+
+		expect(deleteVisitors[0].name).toEqual(newName);
+		expect(deleteVisitors[0].age).toEqual(newAge);
+		expect(deleteVisitors[0].date_of_visit).toEqual(visitor.date);
+		expect(deleteVisitors[0].time_of_visit).toEqual(visitor.time);
+		expect(deleteVisitors[0].assistant).toEqual(visitor.assistant);
+		expect(deleteVisitors[0].comments).toEqual(newComments);
+	}); 
+
+	fit('Should delete all visitors', async() => {
+		const allVisitors = await deleteAllVisitors();
+
+		expect(allVisitors).toEqual([]);
 	});
 
 });
--- src/app.js
+++ src/app.js
@@ -1,70 +1,79 @@
 require('dotenv').config();
 
-const Pool = require("pg").Pool;
-const pool = new Pool();
+const Client = require("pg").Client;
+const client = new Client();
+
+client.connect();
 
 const addNewVisitor = async (visitor_name, visitor_age, date_of_visit, time_of_visit, assistant, comments) => {
 
-	const sql = 'INSERT INTO visitors (name, age, date_of_visit, time_of_visit, assistant, comments) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
-	const data = [visitor_name, visitor_age, date_of_visit, time_of_visit, assistant, comments];
+	const sql = `
+				INSERT 
+				INTO visitors 
+				(name, age, date_of_visit, time_of_visit, assistant, comments) 
+				VALUES ($1, $2, $3, $4, $5, $6) 
+				RETURNING *
+				`;
 
-	return await pool.query(sql, data);
+	const data = [visitor_name, visitor_age, date_of_visit, time_of_visit, assistant, comments];
+	const res = await client.query(sql, data);
+
+	return res.rows;
 }
 
 const listAllVisitors = async () => {
 
 	const sql = 'SELECT * FROM visitors';
+	const res = await client.query(sql);
 
-	return await pool.query(sql);
+	return res.rows;
 }
 
 const deleteVisitor = async id => {
 
 	const sql = 'DELETE FROM visitors WHERE id = $1 RETURNING *';
 	const data = [id];
+	const res = await client.query(sql, data);
 
-	return await pool.query(sql, data);
+	return res.rows;
 }
 
-const updateVisitor = (id, visitor_name, visitor_age, date_of_visit, time_of_visit, assistant, comments) => {
+const updateVisitor = async (id, visitor_name, visitor_age, date_of_visit, time_of_visit, assistant, comments) => {
 
-	const sql = 'UPDATE visitors SET name = $2, age = $3, date_of_visit = $4, time_of_visit = $5, assistant = $6, comments = $7 WHERE id = $1';
+	const sql = `
+				UPDATE 
+				visitors SET 
+				name = $2, 
+				age = $3, 
+				date_of_visit = $4, 
+				time_of_visit = $5, 
+				assistant = $6, 
+				comments = $7 
+				WHERE id = $1 
+				RETURNING *
+				`;
+
 	const data = [id, visitor_name, visitor_age, date_of_visit, time_of_visit, assistant, comments];
+	const res = await client.query(sql, data);
 
-	pool.query(sql, data, (err, res) => {
-		if (err) throw err;
-
-		// Results
-		console.log(res.rows);
-		pool.end();
-	});
+	return res.rows;
 }
 
-const viewOneVisitor = id => {
+const viewOneVisitor = async id => {
 
 	const sql = 'SELECT * FROM visitors WHERE id = $1';
 	const data = [id];
+	const res = await client.query(sql, data);
 
-	pool.query(sql, data, (err, res) => {
-		if (err) throw err;
-
-		// Results
-		console.log(res.rows);
-		pool.end();
-	});
+	return res.rows;
 }
 
-const deleteAllVisitors = () => {
+const deleteAllVisitors = async () => {
 
-	const sql = 'DELETE FROM visitors';
+	const sql = 'DELETE FROM visitors RETURNING *';
+	const res = await client.query(sql);
 
-	pool.query(sql, (err, res) => {
-		if (err) throw err;
-
-		// Results
-		console.log(res.rows);
-		pool.end();
-	});
+	return res.rows;
 }
 
 module.exports = {
